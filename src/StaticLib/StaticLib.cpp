6cdc49dde751028cc233c34eb3fbe3f7554b7395
yuhi610 <bird9.yuhi@gmail.com>
2022-10-04 20:51:16 +0900

    エラーを修正

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -17,7 +17,7 @@ bool is_same(int val1, int val2)
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
 	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
-	int comb[7]={val1, val2, val3, val1+val2, val1+val3, val2+val3, val1+val2+val3}
+	int comb[7]={val1, val2, val3, val1+val2, val1+val3, val2+val3, val1+val2+val3};
 	int flag=0;
 	for(int i=0;i<7;i++){
 		if(sum==comb[i]){flag++;}
