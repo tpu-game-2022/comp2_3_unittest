47f0ff5fb4dcebe5cecc9372bf31b88bfab91959
Hayato222 <g2227070@st.t-kougei.ac.jp>
2022-10-24 14:54:55 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,12 +6,35 @@
 bool is_same(int val1, int val2)
 {
 	// やること：この処理を数字を比較するように変更します
-	return false;
+	return val1==val2;
 }
 
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
 	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
-	return 0;
+	int count = 0;
+	if (sum == val1)
+		count++;
+
+	if (sum == val2)
+		count++;
+
+	if (sum == val3)
+		count++;
+
+	if (sum == val1 + val2)
+		count++;
+
+	if (sum == val2 + val3)
+		count++;
+
+	if (sum == val3 + val1)
+		count++;
+
+	if (sum == val1 + val2 + val3)
+		count++;
+
+	
+	return count;
 }
