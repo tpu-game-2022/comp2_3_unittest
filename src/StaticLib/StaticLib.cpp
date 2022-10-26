6be73a9a3888a8502a94212510c35801e4de2551
“0606T401“ <g2227053@st.t-kougei.ac.jp>
2022-10-27 00:42:57 +0900

    ユニットテストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,12 +6,39 @@
 bool is_same(int val1, int val2)
 {
 	// やること：この処理を数字を比較するように変更します
+	if (val1 == val2) {
+		return true;
+	}
+	else 
 	return false;
 }
 
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
+	int count = 0;
 	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
+	if (sum == val1) {
+		count++;
+	}
+	if (sum == val2) {
+		count++;
+	}
+	if (sum == val3) {
+		count++;
+	}
+	if (sum == val1 + val2) {
+		count++;
+	}
+	if (sum == val1 + val3) {
+		count++;
+	}
+	if (sum == val2 + val3) {
+		count++;
+	}
+	if (sum == val1 + val2 + val3) {
+		count++;
+	}
+	return count;
 	return 0;
 }
